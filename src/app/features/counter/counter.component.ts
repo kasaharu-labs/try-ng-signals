111818674c3c1763b97ab35be365ca20dd00ffaa
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-11 19:56:28 +0900

    feat: signal を使った counter 実装

--- src/app/features/counter/counter.component.ts
+++ src/app/features/counter/counter.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  counter = signal(0);
+
+  incrementCount(): void {
+    this.counter.set(this.counter() + 1);
+  }
+
+  decrementCount(): void {
+    this.counter.update(() => this.counter() - 1);
+  }
+
+  now(): Date {
+    return new Date();
+  }
+}
