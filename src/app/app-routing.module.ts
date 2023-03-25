e97958be38420447a50b9b9fdcc5417615bc65d2
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-25 11:00:09 +0900

    feat: DashboardPageComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardPageComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
 ];
