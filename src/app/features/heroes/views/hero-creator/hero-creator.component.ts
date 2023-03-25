68b67c038137734a045bd78e0c813fdeb88b0d21
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-25 17:29:51 +0900

    npm run ng g c features/heroes/views/hero-creator

---
+++ src/app/features/heroes/views/hero-creator/hero-creator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-creator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-creator.component.html',
+  styleUrls: ['./hero-creator.component.scss']
+})
+export class HeroCreatorComponent {
+
+}
