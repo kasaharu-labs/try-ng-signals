36897c33060b0e93ea4c30590d122dd583948483
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-25 14:46:11 +0900

    npm run ng g c features/dashboard/views/dashboard-content

---
+++ src/app/features/dashboard/views/dashboard-content/dashboard-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-content',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-content.component.html',
+  styleUrls: ['./dashboard-content.component.scss']
+})
+export class DashboardContentComponent {
+
+}
