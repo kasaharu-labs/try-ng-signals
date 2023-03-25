044558b45e3ac44ea00cbd9217b172b5f89bd436
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-25 13:33:48 +0900

    feat: DashboardComponent の ComponentStore を作成

---
+++ src/app/features/dashboard/containers/dashboard/dashboard.store.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Hero } from '../../../../hero';
+
+interface State {
+  heroes: Hero[];
+}
+
+const initialState: State = {
+  heroes: [],
+};
+
+@Injectable()
+export class DashboardStore extends ComponentStore<State> {
+  constructor() {
+    super(initialState);
+  }
+
+  readonly heroes$ = this.select((state) => state.heroes);
+  readonly setHeroes = this.updater((state, heroes: Hero[]) => ({ ...state, heroes }));
+}
