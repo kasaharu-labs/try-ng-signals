4728e756d3023ae502081015717d031ad73a4840
Wataru.Kasahara <kasahara1019@gmail.com>
2023-03-25 16:20:13 +0900

    feat: HeroesPageComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './features/dashboard/pages/dashboard/dashboard.component';
+import { HeroesPageComponent } from './features/heroes/pages/heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './features/heroes/containers/heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardPageComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes', component: HeroesPageComponent },
 ];
 
 @NgModule({
