be510d9c4f203eb9fee60b8db101530c5bbb5f15
Wataru.Kasahara <kasahara1019@gmail.com>
2023-04-22 12:49:01 +0900

    refactor: standalone API を使ってルーティングの設定をおこなうようにした

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { DashboardPageComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 import { HeroesPageComponent } from './features/heroes/pages/heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [
+export const ROUTES: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardPageComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesPageComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { InMemoryDataService } from './app/in-memory-data.service';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { AppRoutingModule } from './app/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppComponent } from './app/app.component';
+import { InMemoryDataService } from './app/in-memory-data.service';
+import { ROUTES } from './app/routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(ROUTES),
     importProvidersFrom(
       BrowserModule,
       FormsModule,
-      AppRoutingModule,
       // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
       // and returns simulated server responses.
       // Remove it when a real server is ready to receive requests.
